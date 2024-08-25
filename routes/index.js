d7abb46564d416c57587e663cd0f3f56955e9bb2
Mirza <127849850+mirzahafizh@users.noreply.github.com>
2024-08-25 15:53:31 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const teamRoute = require('./team.route');
 const contactRoute = require('./contact.route'); // Import the contact route
 const imagekitRoutes = require('./routes/imagekit.route');
 
+
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); /
 route.get("/", (req, res) => {
     res.json("success");
 });
